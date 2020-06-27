9a01b5cab2f6fc8ec362b8c82131e1de090767aa
Amr Elwakeel <amrelwakeel96@hotmail.com>
2020-06-27 03:13:45 +0200

    api

---
+++ ERP/Controllers/CasherController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DAL;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ERP.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CasherController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CasherController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        [HttpPost]
+        public async Task<IActionResult> ChangeDepartment(int Casher,int Dept)
+        {
+            _unitOfWork.CasherRepository.ChangeCasherDepartment(Casher, Dept);
+            if (await _unitOfWork.SaveChanges())
+            {
+                return Ok();
+            }
+            return BadRequest();
+        }
+    }
+}
