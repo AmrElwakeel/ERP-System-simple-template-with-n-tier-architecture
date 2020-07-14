2263141bc77debee7d4ff6e62a642d4e2a0aa5b5
Amr Elwakeel <amrelwakeel96@hotmail.com>
2020-07-14 15:20:13 +0200

    clientApp

--- ERP/ClientApp/src/app/Services/DepartmentServices/department.service.ts
+++ ERP/ClientApp/src/app/Services/DepartmentServices/department.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
 import { Department } from '../../Models/department';
+
+import {map, catchError, tap}  from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +12,25 @@ export class DepartmentService {
 
   URL : string='https://localhost:44302/api/department';
   constructor(private http:HttpClient) { }
+
   public getDepartment():Observable<Department[]>{
-    return this.http.get<Department[]>(this.URL);
+    return this.http.get<Department[]>(this.URL)
+    .pipe(
+      tap(_=>console.log('Fetching departments data.....')),
+      catchError(this.handleError<Department[]>('getDepartments',[]))
+    );
   }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return(errorResponse:HttpErrorResponse):Observable<T>=>{
+      if(errorResponse.error instanceof ErrorEvent){
+        console.error('Client side error : ',errorResponse.error.message);
+      }else{
+        console.error('Server side error : ',errorResponse.error);
+      }
+      return of(result as T);
+    }
+  }
+
+
 }
